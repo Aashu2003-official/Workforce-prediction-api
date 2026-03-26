0f645cc8e1c3745bbdc902137694fe7e3786ba12
Aashu2003-official <aashuofficial2003@gmail.com>
2026-03-26 13:05:19 +0530

    Fix model loading for deployment

--- frontend.py
+++ frontend.py
@@ -12,18 +12,25 @@ emp_2023 = st.number_input("Employees 2023")
 emp_2024 = st.number_input("Employees 2024")
 
 if st.button("Predict"):
-    response = requests.post(
-        "https://ai-workforce-prediction.onrender.com/predict",
-        json={
-            "Emp_2018": emp_2018,
-            "Emp_2019": emp_2019,
-            "Emp_2020": emp_2020,
-            "Emp_2021": emp_2021,
-            "Emp_2022": emp_2022,
-            "Emp_2023": emp_2023,
-            "Emp_2024": emp_2024
-        }
-    )
+    try:
+        response = requests.post(
+             "https://ai-workforce-prediction.onrender.com/predict",
+            json={
+                "emp_2018": emp_2018,
+                "emp_2019": emp_2019,
+                "emp_2020": emp_2020,
+                "emp_2021": emp_2021,
+                "emp_2022": emp_2022,
+                "emp_2023": emp_2023,
+                "emp_2024": emp_2024
+            }
+        )
 
-    result = response.json()
-    st.success(f"Predicted Employees (2025): {result['predicted_2025']}")
+        result = response.json()
+
+        st.write("API Response:", result)  # debug
+
+        st.success(f"Predicted Employees (2025): {result['predicted_2025']}")
+
+    except Exception as e:
+        st.error(f"Error: {e}")
